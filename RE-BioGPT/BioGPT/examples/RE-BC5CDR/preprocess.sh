2ee7e4478317cc4fa0e11a69c6e95a0e5b2c6d52
paswam <paswam@iu.edu>
2023-02-25 17:31:22 -0500

    update

--- RE-BioGPT/BioGPT/examples/RE-BC5CDR/preprocess.sh
+++ RE-BioGPT/BioGPT/examples/RE-BC5CDR/preprocess.sh
@@ -6,6 +6,10 @@ prefix=relis
 RAW_DATA_DIR=${DATA_DIR}/raw
 OUTPUT_DIR=${DATA_DIR}/${prefix}-bin
 
+ROOT=/N/project/zhangclab/Pavi/Jack/Diet-Nutrition-and-Foods-Recommendation/RE-BioGPT/BioGPT
+MOSE=${ROOT}/mosesdecoder
+FASTBPE=${ROOT}/fastBPE/
+
 if [ -d "${OUTPUT_DIR}" ]; then
     rm -rf ${OUTPUT_DIR}
 fi
@@ -21,11 +25,11 @@ for ff in ${SPLIT[@]}; do
     if [ -f "${RAW_DATA_DIR}/${prefix}_$ff.y" ]; then
         echo "Preprocessing ${ff}"
 
-        perl /N/u/paswam/Carbonate/Desktop/Link\ to\ Pavi/Jack/Diet-Nutrition-and-Foods-Recommendation/RE-BioGPT/BioGPT/mosesdecoder/scripts/tokenizer/tokenizer.perl -l en -a -threads 8 < ${RAW_DATA_DIR}/${prefix}_$ff.x > ${RAW_DATA_DIR}/${prefix}_$ff.tok.x
-        perl /N/u/paswam/Carbonate/Desktop/Link\ to\ Pavi/Jack/Diet-Nutrition-and-Foods-Recommendation/RE-BioGPT/BioGPT/mosesdecoder/scripts/tokenizer/tokenizer.perl -l en -a -threads 8 < ${RAW_DATA_DIR}/${prefix}_$ff.y > ${RAW_DATA_DIR}/${prefix}_$ff.tok.y
+        perl ${MOSE}/scripts/tokenizer/tokenizer.perl -l en -a -threads 8 < ${RAW_DATA_DIR}/${prefix}_$ff.x > ${RAW_DATA_DIR}/${prefix}_$ff.tok.x
+        perl ${MOSE}/scripts/tokenizer/tokenizer.perl -l en -a -threads 8 < ${RAW_DATA_DIR}/${prefix}_$ff.y > ${RAW_DATA_DIR}/${prefix}_$ff.tok.y
 
-        /N/u/paswam/Carbonate/Desktop/Link\ to\ Pavi/Jack/Diet-Nutrition-and-Foods-Recommendation/RE-BioGPT/BioGPT/fastBPE/fast applybpe ${RAW_DATA_DIR}/${prefix}_$ff.tok.bpe.x ${RAW_DATA_DIR}/${prefix}_$ff.tok.x ${RAW_DATA_DIR}/bpecodes
-        /N/u/paswam/Carbonate/Desktop/Link\ to\ Pavi/Jack/Diet-Nutrition-and-Foods-Recommendation/RE-BioGPT/BioGPT/fastBPE/fast applybpe ${RAW_DATA_DIR}/${prefix}_$ff.tok.bpe.y ${RAW_DATA_DIR}/${prefix}_$ff.tok.y ${RAW_DATA_DIR}/bpecodes
+        ${FASTBPE}/fast applybpe ${RAW_DATA_DIR}/${prefix}_$ff.tok.bpe.x ${RAW_DATA_DIR}/${prefix}_$ff.tok.x ${RAW_DATA_DIR}/bpecodes
+        ${FASTBPE}/fast applybpe ${RAW_DATA_DIR}/${prefix}_$ff.tok.bpe.y ${RAW_DATA_DIR}/${prefix}_$ff.tok.y ${RAW_DATA_DIR}/bpecodes
 
         rm ${RAW_DATA_DIR}/${prefix}_$ff.tok.x ${RAW_DATA_DIR}/${prefix}_$ff.tok.y
     fi
